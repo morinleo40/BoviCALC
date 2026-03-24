834b05aae0006015382459c487a818b2f7824a83
morinleo40 <morinleo40@gmail.com>
2026-03-24 10:15:17 +0100

    Add files via upload

--- aliments_db.js
+++ aliments_db.js
@@ -1,53 +1,51 @@
 // Base d'aliments embarquée (verrouillée) — générée depuis Prog/aliments.csv
 // Objectif: fonctionnement 100% offline (double-clic sur index.html).
-window.RB_ALIMENTS_CSV = `Nom,MS,UFL INRA 2018 par kg,MAT %,PDI INRA 2018 g/kg
-Avoine,87.6,0.87,9.4,64.0
-Avoine décortiquée,85.7,1.0,11.0,71.0
-Avoine floconnée,87.6,0.87,9.4,68.0
-Blé dur,87.8,1.06,14.4,84.0
-Blé tendre,86.9,1.04,11.0,77.0
-Maïs,86.3,1.11,7.6,82.0
-Maïs extrudé,86.3,1.11,7.6,93.0
-Maïs floconné,86.3,1.11,7.6,96.0
-Maïs humide,67.0,0.86,6.2,53.0
-Mil,89.6,1.18,11.2,97.0
-Millet commun,90.6,1.2,12.9,78.0
-Orge,87.2,0.97,9.9,76.0
-Riz cargo,88.0,1.06,9.2,71.0
-Riz paddy,88.0,0.76,7.5,59.0
-"Riz poli, brisures",87.6,1.08,8.1,71.0
-Seigle,86.7,1.04,8.5,70.0
-Sorgho,87.8,1.06,9.3,86.0
-Triticale,86.8,1.02,10.0,74.0
-"Tourteau de canola, huile < 5 %",90.4,0.94,37.0,154.0
-"Tourteau de colza, huile 5-20 %",92.0,1.1,31.0,123.0
-"Tourteau de colza, huile < 5%",89.0,0.88,33.9,128.0
-"Tourteau de lin, huile < 5%",87.9,0.9,32.0,162.0
-"Tourteau de lin, huile > 5%",91.2,1.04,32.3,164.0
-"Tourteau de soja, huile 5-20 %",93.2,1.27,43.8,194.0
-"Tourteau de soja, huile < 5%, 46% protéine + huile",87.7,1.1,43.5,191.0
-"Tourteau de soja, huile < 5%, 48% protéine + huile",88.0,1.11,46.2,200.0
-"Tourteau de soja, huile < 5%, 48% protéine + huile, extrudé",88.0,1.11,46.2,211.0
-"Tourteau de soja, huile < 5%, 48% protéine + huile, tanné au formol",88.0,1.11,46.2,334.0
-"Tourteau de soja, huile < 5%, 50% protéine + huile",88.0,1.12,48.5,209.0
-"Farine de blé dur, gruau D",86.6,1.05,16.1,85.0
-"Farine de maïs, cellulose brute < 2 %",87.6,1.16,7.1,82.0
-"Farine fourragère de maïs, cellulose brute 2-10 %",87.4,1.16,9.1,88.0
-Betterave fourragère fraîche,16.1,0.17,1.3,14.0
-Pulpe de betterave déshydratée,88.9,0.92,7.9,82.0
-Pulpe de betterave surpressée,24.3,0.25,2.1,21.0
-"Luzerne déshydratée, protéines 16-18 % sur sec",90.8,0.66,15.4,88.0
-"Luzerne déshydratée, protéines 17-19 % sur sec",90.6,0.68,16.2,91.0
-"Luzerne déshydratée, protéines 19-22 % sur sec",90.8,0.72,18.3,97.0
-"Luzerne déshydratée, protéines 22-25 % sur sec",90.4,0.76,20.8,109.0
-"Luzerne déshydratée, protéines < 16 % sur sec",91.3,0.66,14.1,85.0
-"Luzerne déshydratée, protéines > 25 % sur sec",90.4,0.74,23.8,118.0
-Mélasse de betterave,75.4,0.72,10.7,52.0
-Mélasse de canne,72.3,0.57,4.2,42.0
+window.RB_ALIMENTS_CSV = `Nom,MS,UFL INRA 2018 par kg,MAT %,PDI INRA 2018 g/kg,BACA
+Avoine,87.6,0.87,9.4,64.0,-1
+Avoine décortiquée,85.7,1.0,11.0,71.0,-22
+Avoine floconnée,87.6,0.87,9.4,68.0,-1
+Blé dur,87.8,1.06,14.4,84.0,4
+Blé tendre,86.9,1.04,11.0,77.0,-9
+Maïs,86.3,1.11,7.6,82.0,-0.9
+Maïs extrudé,86.3,1.11,7.6,93.0,-0.9
+Maïs floconné,86.3,1.11,7.6,96.0,-0.9
+Maïs humide,67.0,0.86,6.2,53.0,3
+Mil,89.6,1.18,11.2,97.0,34
+Millet commun,90.6,1.2,12.9,78.0,34
+Orge,87.2,0.97,9.9,76.0,21
+Riz cargo,88.0,1.06,9.2,71.0,69
+Riz paddy,88.0,0.76,7.5,59.0,54
+"Riz poli, brisures",87.6,1.08,8.1,71.0,31
+Seigle,86.7,1.04,8.5,70.0,25
+Sorgho,87.8,1.06,9.3,86.0,34
+Triticale,86.8,1.02,10.0,74.0,27
+"Tourteau de canola, huile < 5 %",90.4,0.94,37.0,154.0,-166
+"Tourteau de colza, huile 5-20 %",92.0,1.1,31.0,123.0,-200
+"Tourteau de colza, huile < 5%",89.0,0.88,33.9,128.0,-165
+"Tourteau de lin, huile < 5%",87.9,0.9,32.0,162.0,84
+"Tourteau de lin, huile > 5%",91.2,1.04,32.3,164.0,68
+"Tourteau de soja, huile 5-20 %",93.2,1.27,43.8,194.0,371
+"Tourteau de soja, huile < 5%, 46% protéine + huile",87.7,1.1,43.5,191.0,336
+"Tourteau de soja, huile < 5%, 48% protéine + huile",88.0,1.11,46.2,200.0,325
+"Tourteau de soja, huile < 5%, 48% protéine + huile, extrudé",88.0,1.11,46.2,211.0,325
+"Tourteau de soja, huile < 5%, 48% protéine + huile, tanné au formol",88.0,1.11,46.2,334.0,325
+"Tourteau de soja, huile < 5%, 50% protéine + huile",88.0,1.12,48.5,209.0,311
+"Farine de blé dur, gruau D",86.6,1.05,16.1,85.0,87
+"Farine de maïs, cellulose brute < 2 %",87.6,1.16,7.1,82.0,-53
+"Farine fourragère de maïs, cellulose brute 2-10 %",87.4,1.16,9.1,88.0,73
+Betterave fourragère fraîche,16.1,0.17,1.3,14.0,978
+Pulpe de betterave déshydratée,88.9,0.92,7.9,82.0,-28
+Pulpe de betterave surpressée,24.3,0.25,2.1,21.0,133
+"Luzerne déshydratée, protéines 16-18 % sur sec",90.8,0.66,15.4,88.0,125
+"Luzerne déshydratée, protéines 17-19 % sur sec",90.6,0.68,16.2,91.0,333
+"Luzerne déshydratée, protéines 19-22 % sur sec",90.8,0.72,18.3,97.0,345
+"Luzerne déshydratée, protéines 22-25 % sur sec",90.4,0.76,20.8,109.0,123
+"Luzerne déshydratée, protéines < 16 % sur sec",91.3,0.66,14.1,85.0,214
+"Luzerne déshydratée, protéines > 25 % sur sec",90.4,0.74,23.8,118.0,352
+Mélasse de betterave,75.4,0.72,10.7,52.0,1109
+Mélasse de canne,72.3,0.57,4.2,42.0,319
 Mélasse de soja,57.6,0.41,4.4,34.0
-Paille de blé,90.3,0.45,3.5,50.0
-Huile de palme,99.8,3.61,0.0,0.0
-ffgfbg,25.0,0.5,15.0,15.0
-frdfb,35.0,0.99,25.0,35.0
+Paille de blé,90.3,0.45,3.5,50.0,78
+Huile de palme,99.8,3.61,0.0,0.0,0
 `;
 
